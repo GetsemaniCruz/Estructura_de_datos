a092db65f5b3e0da68088896df282f6a73c2bb69
Getsemaní Cruz Millán <20243ds149@utez.edu.mx>
2025-10-13 16:17:36 -0600

    insertar

--- Insertar/src/App.java
+++ Insertar/src/App.java
@@ -12,26 +12,26 @@ public class App {
         int[][] arrNu = new int[arrNormal.length][];
         
         for (int i = 0; i < arrNormal.length; i++) {
-            arrNu[i] = new int[arrNormal[i].length + 1];
+            arrNu[i] = new int[arrNormal[i].length +1];
             for (int j = 0; j < arrNormal[i].length; j++) {
                 arrNu[i][j] = arrNormal[i][j];
             }
 
             if(arrNormal[i].length % 2 == 0){
                 int primero = arrNormal[i][0];
-                int ultimo = arrNormal[i][arrNormal[i].length - 1];
-                arrNu[i][arrNu[i].length - 1] = primero + ultimo;
+                int ultimo = arrNormal[i][arrNormal[i].length -1];
+                arrNu[i][arrNu[i].length -1]=primero+ultimo;
                 } else {
-                arrNu[i][arrNu[i].length - 1] = 0;
+                arrNu[i][arrNu[i].length -1]= 0;
             }
         }
 
         for (int i = 0; i < arrNu.length; i++) {
             for (int j = 0; j < arrNu[i].length; j++) {
                 System.out.print(arrNu[i][j]);
-                if (j < arrNu[i].length - 1) System.out.print(" ");;
+                if (j < arrNu[i].length -1) System.out.print(" ");;
             }
-            System.out.println("  "); 
+            System.out.println(" "); 
         }
        
     }
